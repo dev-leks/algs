2ee515ecfeaf45fa8eec76d343dcb0e31a0e8534
Oleksandr Drobot <oleksandr_drobot@epam.com>
2025-07-09 18:47:58 +0200

    LC 1679 max number of k-sum pairs

---
+++ LeetCode/LC_1679_maxNumOfKSumPairs.ts
@@ -0,0 +1,44 @@
+// Two pointers, Medium
+// Space: O(1)
+// Time: O(n log n), because of the sort
+function maxOperationsTP(nums: number[], k: number): number {
+    nums.sort((a, b) => a - b);
+   
+    let operations = 0;
+    let leftP = 0;
+    let rightP = nums.length - 1;
+
+    while (leftP < rightP) {
+        const sum = nums[leftP] + nums[rightP];
+
+        if (sum === k) {
+            operations++;
+            leftP++;
+            rightP--;
+        } else if (sum > k) rightP--;
+        else leftP++;
+    }
+
+    return operations;
+};
+
+// Hash table, Medium
+// Space: O(n)
+// Time: O(n)
+function maxOperationsHT(nums: number[], k: number): number {
+    const numCounts = new Map();
+    let operations = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        const diff = k - nums[i];
+
+        if (numCounts.get(diff)) {
+            operations++;
+            numCounts.set(diff, numCounts.get(diff) - 1);
+        } else {
+            numCounts.set(nums[i], (numCounts.get(nums[i]) ?? 0) + 1);
+        }
+    }
+
+    return operations;
+};
