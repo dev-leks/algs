121e77d299bfbd65d924a2086897c90e5570b63f
Oleksandr Drobot <oleksandr_drobot@epam.com>
2025-07-15 13:57:10 +0200

    LC 2352 equal row and column pairs

---
+++ LeetCode/LC_2352_equalRowAndColPairs.ts
@@ -0,0 +1,78 @@
+// Array, Hashmap, Medium (Optimized)
+// Time: O(n^2)
+// Space: O(n)
+function equalPairs(grid: number[][]): number {
+    const rowFrequencyMap = new Map();
+
+    for (let rowId = 0; rowId < grid.length; rowId++) {
+        const row = grid[rowId].join('|');
+        rowFrequencyMap.set(row, (rowFrequencyMap.get(row) ?? 0) + 1);
+    }
+
+    let pairsCount = 0;
+
+    for (let columnId = 0; columnId < grid[0].length; columnId++) {
+        const column: number[] = [];
+        for (let rowId = 0; rowId < grid.length; rowId++) {
+            column.push(grid[rowId][columnId]);
+        }
+        const columnHash = column.join('|');
+
+        const frequency = rowFrequencyMap.get(columnHash);
+        if (frequency) pairsCount += frequency;
+    }
+
+    return pairsCount;
+};
+
+// Array, Hashmap, Medium (my first solution)
+// Time: O(n^2)
+// Space: O(n^2)
+function equalPairs1(grid: number[][]): number {
+    const rowsMap = new Map();
+    const columnsMap = new Map();
+
+    for (let rowI = 0; rowI < grid.length; rowI++) {
+        for (let columnI = 0; columnI < grid[rowI].length; columnI++) {
+            const row = rowsMap.get(rowI);
+            rowsMap.set(rowI, (row ? row + ',' : '') + grid[rowI][columnI]);
+            const column = columnsMap.get(columnI);
+            columnsMap.set(columnI, (column ? column + ',' : '') + grid[rowI][columnI]);
+        }
+    }
+
+    let pairsCount = 0;
+    rowsMap.forEach((row) => {
+        columnsMap.forEach((column) => {
+            if (row === column) pairsCount++;
+        })
+    });
+
+    return pairsCount;
+};
+
+// Array, Hashmap, Medium (Optimized with help)
+// Time: O(n^2)
+// Space: O(n^2)
+function equalPairs2(grid: number[][]): number {
+    const rowFrequencyMap = new Map();
+    const columns: string[] = [];
+
+    for (let rowId = 0; rowId < grid.length; rowId++) {
+        const row = grid[rowId].join('|');
+        rowFrequencyMap.set(row, (rowFrequencyMap.get(row) ?? 0) + 1);
+
+        for (let columnId = 0; columnId < grid[rowId].length; columnId++) {
+            const column = String(grid[rowId][columnId]);
+            columns[columnId] = columns[columnId] ? columns[columnId] + '|' + column : column;
+        }
+    }
+
+    let pairsCount = 0;
+    for (const column of columns) {
+        const frequency = rowFrequencyMap.get(column);
+        if (frequency) pairsCount += frequency;
+    }
+
+    return pairsCount;
+};
