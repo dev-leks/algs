4c73bdea08d672c2cee0deaddccc942841bb0007
dev-leks <alex.drobot22@gmail.com>
2025-07-21 10:23:21 +0200

    LC 328 odd even linked list

---
+++ LeetCode/LC_328_oddEvenLinkedList.ts
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+// Linked list, 2 pointers, Medium (With help)
+// Time: o(n)
+// Space: O(1)
+function oddEvenList(head: ListNode | null): ListNode | null {
+    if (!head || !head.next) return head;
+
+    let odd = head;
+    let even = head.next;
+    const evenHead = even;
+   
+    while (even && even.next) {
+        odd.next = even.next;
+        odd = odd.next;
+        
+        even.next = odd.next;
+        even = even.next;
+    }
+
+    odd.next = evenHead;
+
+    return head;
+};
+
+// Linked List, Medium (My first solution)
+// Time: O(n)
+// Space: O(1)
+function oddEvenList1(head: ListNode | null): ListNode | null {
+    if (!head || !head.next) return head;
+
+    let count = 0;
+    let tempHead = head;
+    while (tempHead) {
+        count++;
+        tempHead = tempHead.next;
+    }
+
+    let originalHead = head;
+    let originalSecondNode = head.next;
+
+    while (head) {
+        const nextNode = head.next;
+       
+        if (!nextNode) head.next = count % 2 == 0 ? null : originalSecondNode;
+        else if (!nextNode.next) head.next = count % 2 == 0 ? originalSecondNode : null;
+        else head.next = nextNode.next;
+      
+        head = nextNode;
+    }
+
+    return originalHead;
+};
