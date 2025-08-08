2158065069e8d56a2f0e747bb85a23910d7c6ae1
Oleksandr Drobot <oleksandr_drobot@epam.com>
2025-08-08 18:58:28 +0200

    LC 450 delete node in a binary search tree

---
+++ LeetCode/LC_450_deleteNodeInBST.ts
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// DFS, Easy (With little help)
+// Time: O(n)
+// Space: O(n)
+function deleteNode(root: TreeNode | null, key: number): TreeNode | null {
+    if (!root) return null;
+
+    function getSuccessor(node: TreeNode): TreeNode | null {
+        let curr = node.right;
+
+        while (curr !== null && curr.left !== null) curr = curr.left;
+
+        return curr;
+    }
+
+    if (root.val === key) {
+        if (!root.left) return root.right;
+
+        if (!root.right) return root.left;
+
+        const successor = getSuccessor(root);
+        root.val = successor!.val;
+        root.right = deleteNode(root.right, successor!.val);
+    }
+
+    if (root.val > key) root.left = deleteNode(root.left, key);
+    else root.right = deleteNode(root.right, key);
+
+    return root;
+};
