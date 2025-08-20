39f9b30fbac60eaf99a1273e36cb28cddcb69b8d
Oleksandr Drobot <oleksandr_drobot@epam.com>
2025-08-20 23:52:09 +0200

    LC 2542 maximum subsequence score

---
+++ LeetCode/LC_2542_maxSubsequenceScore.ts
@@ -0,0 +1,22 @@
+// Greedy + Heap, Medium
+// Time: O(nlog)
+// Space: O(n + k)
+function maxScore(nums1: number[], nums2: number[], k: number): number {
+    const pairs = nums1.map((num, index) => [num, nums2[index]]);
+    pairs.sort((a, b) => b[1] - a[1]);
+
+    let score = 0;
+    let sum = 0;
+    const heap = new MinPriorityQueue<number>();
+
+    for (const [num1, num2] of pairs) {
+        heap.enqueue(num1);
+        sum += num1;
+
+        if (heap.size() > k) sum -= heap.dequeue()!;
+
+        if (heap.size() === k) score = Math.max(score, sum * num2);
+    }
+
+    return score;
+};
