c0b60b89f3e5c050fb91a5f1bfc47f55eea0664c
Oleksandr Drobot <oleksandr_drobot@epam.com>
2025-10-29 10:30:24 +0100

    update LC 62

--- LeetCode/LC_62_uniquePaths.ts
+++ LeetCode/LC_62_uniquePaths.ts
@@ -4,13 +4,9 @@
 function uniquePaths(m: number, n: number): number {
     const dp: number[][] = Array.from({ length: m }, () => Array(n).fill(1))
 
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (i === 0 || j === 0) {
-                dp[i][j] = 1;
-            } else {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
         }
     }
 
