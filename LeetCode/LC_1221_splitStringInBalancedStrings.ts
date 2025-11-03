915e4899b06117c76fbbf10b6a1421f65586826f
Oleksandr Drobot <oleksandr_drobot@epam.com>
2025-11-03 09:58:15 +0100

    LC 1221 split a string in balanced strings

---
+++ LeetCode/LC_1221_splitStringInBalancedStrings.ts
@@ -0,0 +1,18 @@
+// Greedy, Easy
+// Time: O(n)
+// Space: O(1)
+function balancedStringSplit(s: string): number {
+  let count = 0;
+  let curr = 0;
+
+  for (const char of s) {
+    curr += (char === 'R' ? 1 : -1);
+
+    if (curr === 0) {
+      count++;
+      curr = 0;
+    }
+  }
+
+  return count;
+};
