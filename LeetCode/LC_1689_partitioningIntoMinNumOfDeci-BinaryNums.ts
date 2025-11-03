ff9e9ea9054af7ac522921cc4493874e614bc1f8
Oleksandr Drobot <oleksandr_drobot@epam.com>
2025-11-04 00:17:20 +0100

    LC 1689 partitioning into minimum number of deci-binary numbers

---
+++ LeetCode/LC_1689_partitioningIntoMinNumOfDeci-BinaryNums.ts
@@ -0,0 +1,16 @@
+// Greedy, Easy (The max digit is the min partition)
+// Time: O(n)
+// Space: O(1)
+function minPartitions(n: string): number {
+  let res = +n[0];
+
+  for (let i = 1; i < n.length; i++) {
+    const digit = +n[i];
+
+    if (digit > res) {
+      res = digit;
+    }
+  }
+
+  return res;
+};
