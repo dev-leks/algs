88de22b321df16bb3b8f215cd92b9c705f23a965
Oleksandr Drobot <oleksandr_drobot@epam.com>
2026-03-05 23:02:22 +0100

    update LC 198

--- LeetCode/LC_198_houseRobber.ts
+++ LeetCode/LC_198_houseRobber.ts
@@ -18,11 +18,15 @@ function rob(nums: number[]): number {
 // Time: O(n)
 // Space: O(n)
 function rob1(nums: number[]): number {
+    if (!nums.length) return 0;
+
+    if (nums.length === 1) return nums[0];
+
     const n = nums.length;
     const dp: number[] = [];
 
     dp[0] = 0;
-    dp[1] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
 
     for (let i = 2; i <= n; i++) {
         dp[i] = Math.max((nums[i - 1] + dp[i - 2]), dp[i - 1]);
@@ -30,3 +34,33 @@ function rob1(nums: number[]): number {
 
     return dp[dp.length - 1];
 };
+
+function rob0(nums: number[]): number {
+    if (!nums.length) return 0;
+
+    const memo = new Map<number, number>();
+
+    function dp(index: number): number {
+        if (memo.has(index)) {
+            return memo.get(index)!;
+        }
+
+        if (index === 0) {
+            memo.set(0, nums[0]);
+            return nums[0];
+        }
+
+        if (index === 1) {
+            const val = Math.max(nums[0], nums[1]);
+            memo.set(1, val);
+            return val;
+        }
+
+        const val = Math.max(nums[index] + dp(index - 2), dp(index - 1));
+        memo.set(index, val);
+
+        return val;
+    }
+
+    return dp(nums.length - 1);
+}
